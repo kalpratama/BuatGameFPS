fb608b0a262a44de72139c8d208831afafc4d9b0
Kal EL Pratama <kalpratama@gmail.com>
2024-07-01 20:13:18 +0700

    Add Guns and Shooting script.
    Belom bener2 bisa nembak, target blm diimplementasi. Anim belom bener. Belom bener tp ga ada konflik sama versi sblmnya.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int health = 50;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
